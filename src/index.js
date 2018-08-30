1974113bda2886fc3d60e2ad10c5bb2d77f2fb95
Maciej Ząbek <maciek@maciek.local>
2018-08-30 22:21:46 +0200

    eslint

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import './index.css'
 import numeral from 'numeral';
 
 const value = numeral(1000).format('$0,0.00');
-console.log(`It costs ${value}!`);
+console.log(`It costs ${value}!`); // eslint-disable-line no-console
