5866d11ac08a75f366f69727ec63754d68973eb4
Maciej Ząbek <maciek@maciek.local>
2018-09-27 21:37:46 +0200

    uglyfing js and html

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,11 +3,11 @@ import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
-  devtool: 'inline-source-map',
+  devtool: 'source-map',
   entry: [path.resolve(__dirname, 'src/index')],
   target: 'web',
   output: {
-    path: path.resolve(__dirname, 'src'),
+    path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
     filename: 'bundle.js'
   },
@@ -19,9 +19,25 @@ export default {
     }),
     new HtmlWebpackPlugin({
       template: 'src/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
       inject: true
     })
   ],
+  optimization: {
+    minimize: true
+  },
+
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader'] },
